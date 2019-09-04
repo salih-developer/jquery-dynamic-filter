cc495dcb457a764eedb50d0b8dd9b2349c0d32a7
salihbicakci <salihbicakci@SALIHBICAKCI>
2019-09-04 09:20:21 +0300

    up

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication9.Models;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace WebApplication9.Controllers
 {
